94b15cc0cf4b3f394fe66a42ca57503c37de0cf6
loganlieou <logan.lieou@gmail.com>
2024-01-30 23:34:14 -0600

    added some things

--- rplugin/python3/templates.py
+++ rplugin/python3/templates.py
@@ -2,9 +2,19 @@ import pynvim
 
 @pynvim.plugin
 class Main(object):
-    def __init__(self, vim):
-        self.vim = vim
+    def __init__(self, nvim):
+        self.nvim = nvim
 
-    @pynvim.function("Hello")
-    def helloPy(self, args):
-        self.vim.command('echo "Hello, world!\n"')
+    @pynvim.function('TestFunction', sync=True)
+    def testfunction(self, args):
+        return 3
+
+    @pynvim.command('TestCommand', nargs='*', range='')
+    def testcommand(self, args, range):
+        self.nvim.current.line = ('Command with args: {}, range: {}'
+                                  .format(args, range))
+
+    @pynvim.autocmd('BufEnter', pattern='*.py', eval='expand("<afile>")', sync=True)
+    def on_bufenter(self, filename):
+        self.nvim.out_write('testplugin is in ' + filename + '\n')
+
