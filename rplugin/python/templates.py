c3a9522ebc42bef8a14ae798facc1ba3c26a74ae
loganlieou <logan.lieou@gmail.com>
2024-01-30 23:11:58 -0600

    removed plugin directory

--- rplugin/python/templates.py
+++ rplugin/python/templates.py
@@ -5,12 +5,6 @@ class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    @pynvim.command('Template', range='')
-    def testcommand(self, args):
-        self.vim.current.line = f"Command with args: {args}, range: {range}"
-
-    # testing this autocmd thing prob should configure this to be really good
-    @pynvim.autocmd('BufEnter', pattern='*.py', eval='expand("<afile>")', sync=True)
-    def on_bufenter(self, filename):
-        self.vim.out_write('testplugin is in ' + filename + '\n')
-
+    @pynvim.function("Hello")
+    def helloPy(self, args):
+        self.vim.command('echo "Hello, world!\n"')
